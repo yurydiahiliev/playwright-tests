8694b314756a562eec6bfbb9eee38575bfd64595
Diahiliev, Yurii <Yurii.Diahiliev@McKesson.com>
2023-01-15 13:09:48 +0200

    fixed handler.js

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,31 +1,31 @@
-const {runCLI} = require('jest')
+const { runCLI } = require('jest')
 
 const jestConfig = require('./jest.config')
 
 module.exports.runTest = async event => {
-    try {
-        const {testMatch} = event.body
-        if (typeof testMatch === underfined) {
-            return {
-                statusCode : 400,
-                body : JSON.stringify('No found \'test_match\'.')
-            }
-        }
-
-        const {results} = await runCLI({
-            ...jestConfig,
-            testMatch: [testMatch]
-        }, [''])
-
-        return {
-            statusCode : 200,
-            body : JSON.stringify(results)
-        }
-    } catch (error) {
-        console.log({ error });
-        return {
-            statusCode : 500,
-            body : JSON.stringify(error),
-        }
+  try {
+    const { testMatch } = event.body
+    if (typeof testMatch === undefined) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify('Not found \'testMatch\'.')
+      }
     }
+
+    const { results } = await runCLI({
+      ...jestConfig,
+      testMatch: [testMatch]
+    }, [''])
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results)
+    }
+  } catch (error) {
+    console.error({ error });
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    }
+  }
 }
